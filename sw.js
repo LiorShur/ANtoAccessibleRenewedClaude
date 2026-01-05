ad0578b7bbca28aad1f0e7fac418b02c4b522e88
LiorShur <96306484+LiorShur@users.noreply.github.com>
2026-01-05 11:09:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * - Images: Cache First
  */
 
-const CACHE_VERSION = 'v2.6.1-zindex-fix';
+const CACHE_VERSION = 'v2.6.2-mobile-banner';
 const APP_CACHE = `access-nature-app-${CACHE_VERSION}`;
 const DATA_CACHE = `access-nature-data-${CACHE_VERSION}`;
 const MAP_CACHE = `access-nature-maps-${CACHE_VERSION}`;
